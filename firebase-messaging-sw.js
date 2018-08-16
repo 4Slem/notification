379fb248221fecaa2da883a10ea424ab291ed879
Roman Vakulin <vakulinromansergiyovich@gmail.com>
2018-08-16 22:28:02 +0300

    sdf

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,14 @@
-self.addEventListener('install', function(event) {
-  console.log('install');
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        'index.html'
+      ]);
+    })
+  );
+});
+
+
+this.addEventListener('fetch', function(event) {
+  console.log('fetch');
 });
